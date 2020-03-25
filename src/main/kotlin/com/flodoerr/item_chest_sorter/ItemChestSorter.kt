da7f184545f529a183c5759f0df74d4800c096e9
corylulu <corylulu@gmail.com>
2020-03-25 14:22:24 -0700

    small fix

--- src/main/kotlin/com/flodoerr/item_chest_sorter/ItemChestSorter.kt
+++ src/main/kotlin/com/flodoerr/item_chest_sorter/ItemChestSorter.kt
@@ -97,7 +97,7 @@ class ItemChestSorter: JavaPlugin() {
     fun isItemInSet(itemInChest: ItemStack, itemInItemFrame: ItemStack): Boolean {
         val sets = getSets()
 
-        if(this.config.getBoolean("enabled") == true) {
+        if(this.config.getBoolean("enableSetsRegex") == true) {
             if(setsRegex.count() == 0){
                 // Keeps regex in buffer
                 setsRegex = ArrayList(sets.map {
